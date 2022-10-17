2d2ad225e7e176d59de72944d10c6b160062fea8
Panagiotis Zachos <panzaxos@gmail.com>
2022-10-17 15:27:00 +0300

    Initial commit

---
+++ Assets/Scripts/GUI_Controller.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement; // To change scenes through the menu
+
+public class GUI_Controller : MonoBehaviour
+{
+
+    public Button open_cue_button;
+    public Button save_cue_button;
+    public Button edit_cue_button;
+    public Button save_settings_button;
+
+    public Label scene_hour_label;
+    public Label scene_min_label;
+    public Label scene_sec_label;
+
+    public Label play_hour_label;
+    public Label play_min_label;
+    public Label play_sec_label;
+
+    private float time_start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // SceneManager.LoadScene('name-of-the-scene'); // For future use
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        open_cue_button = root.Q<Button>("open_cue_file");
+        save_cue_button = root.Q<Button>("save_cue_file");
+        edit_cue_button = root.Q<Button>("save_settings");
+        save_settings_button = root.Q<Button>("save_settings");
+
+        scene_hour_label = root.Q<Label>("scene_hours");
+        scene_min_label = root.Q<Label>("scene_minutes");
+        scene_sec_label = root.Q<Label>("scene_seconds");
+
+        play_hour_label = root.Q<Label>("play_hours");
+        play_min_label = root.Q<Label>("play_minutes");
+        play_sec_label = root.Q<Label>("play_seconds");
+
+        time_start = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Time.time - time_start);
+        var frame_time = Time.time;
+        var time_hours = Mathf.Floor((frame_time - time_start) / 3600);
+        var time_minutes = Mathf.Floor((frame_time - time_hours * 3600) / 60);
+        var time_seconds = Mathf.Floor(frame_time - time_hours * 3600 - time_minutes * 60);
+
+        play_hour_label.text = time_hours.ToString();
+        play_min_label.text = time_minutes.ToString();
+        play_sec_label.text = time_seconds.ToString();
+    }
+
+    //private (string, string, string) convertSecondsToTime(float seconds)
+    //{
+    //    var time_hours = Mathf.Floor(seconds / 3600);
+    //    var time_minutes = Mathf.Floor((seconds - time_hours * 3600) / 60);
+    //    var time_seconds = Mathf.Floor(seconds - time_hours * 3600 - time_minutes * 60);
+    //    return "a";
+    //}
+}
