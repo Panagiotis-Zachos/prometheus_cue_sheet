c95e961e47f3b4a13bcf146f35e3a36e933226d3
Panagiotis Zachos <panzaxos@gmail.com>
2022-12-02 16:13:35 +0200

    Remove old libraries

--- Assets/Scripts/GUI_Controller.cs
+++ Assets/Scripts/GUI_Controller.cs
@@ -1,10 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement; // To change scenes through the menu
-using System;
-using UnityEngine.EventSystems;
 
 public class GUI_Controller : MonoBehaviour
 {
